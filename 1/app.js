6ffa632042c321886251edc07581189cb1126db8
Hasan Anıl GÜL <hasananilgul@gmail.com>
2023-08-21 10:31:15 +0300

    diziler

--- 1/app.js
+++ 1/app.js
@@ -1,5 +1,8 @@
-var sayi=0;
+const dizi1 = [1, 2, 3, 4, 5];
+const dizi2 = [3, 4, 5, 6, 7];
 
-// test
-console.log(sayi);
+const ayniOgeler = dizi2.filter(item => dizi1.includes(item));
+const birlesikDizi = dizi1.concat(dizi2);
 
+console.log("Aynı Öğeler: ", ayniOgeler);
+console.log("Birleştirilmiş Dizi: ", birlesikDizi);
