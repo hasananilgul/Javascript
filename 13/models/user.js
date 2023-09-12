e7f4deffd6c918f21027845f0fc62c371a9a75d8
Hasan Anıl GÜL <hasananilgul@gmail.com>
2023-09-12 10:02:18 +0300

    usersBookTask

--- 13/models/user.js
+++ 13/models/user.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
-
-
 const userSchema = new mongoose.Schema({
   username: { 
     type: String,
---
+++ 13/router/user.js
@@ -0,0 +1,104 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user'); // Kullanıcı modelini doğru şekilde dahil edin
+
+// Kayıt ol endpoint'i
+router.post('/kayit', async (req, res) => {
+  const { username, gender, email, phone, password } = req.body;
+
+  // Yeni bir kullanıcı oluştur
+  const newUser = new User({
+    username,
+    gender,
+    email,
+    phone,
+    password,
+  });
+
+  try {
+    // Kullanıcıyı veritabanına kaydet
+    const savedUser = await newUser.save();
+    console.log('Kullanıcı kaydedildi:', savedUser);
+    // Başarılı bir yanıt gönder
+    res.send('Kayıt başarılı.');
+  } catch (err) {
+    // Hata durumunda hata mesajını yakalayın
+    console.error('Kayıt hatası:', err);
+    // Hata yanıtı gönder
+    res.status(500).send('Kayıt sırasında bir hata oluştu.');
+  }
+});
+router.get('/giris', (req, res) => {
+  res.render('giris');
+});
+
+// Giriş endpoint'i
+router.post('/giris', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    // Veritabanından kullanıcıyı arayın
+    const user = await User.findOne({ username });
+
+    if (user) {
+      // Kullanıcıyı bulduysanız, şifreyi karşılaştırın
+      const passwordMatched = await bcrypt.compare(password, user.password);
+
+      if (passwordMatched) {
+        // Giriş başarılı
+        const token = jwt.sign({ username }, 'secretKey');
+        res.send('Giriş başarılı. Token: ' + token);
+      } else {
+        // Şifre uyuşmuyor
+        res.status(401).send('Hatalı şifre.');
+      }
+    } else {
+      // Kullanıcı bulunamadı
+      res.status(401).send('Kullanıcı bulunamadı.');
+    }
+  } catch (err) {
+    console.error('Giriş hatası:', err);
+    res.status(500).send('Giriş işlemi sırasında bir hata oluştu.');
+  }
+});
+
+
+// Kullanıcıyı Silme endpoint'i
+router.delete('/sil/:id', async (req, res) => {
+  try {
+    const userId = req.params.id.trim(); // Gelen id'den gereksiz karakterleri kaldır
+    const deletedUser = await User.findByIdAndRemove(userId);
+    if (deletedUser) {
+      res.send('Kullanıcı başarıyla silindi.');
+    } else {
+      res.status(404).send('Kullanıcı bulunamadı.');
+    }
+  } catch (err) {
+    console.error('Kullanıcı silme hatası:', err);
+    res.status(500).send('Kullanıcı silinirken bir hata oluştu.');
+  }
+});
+
+// Kullanıcı Adını Güncelleme
+router.put('/updateusername/:id', async (req, res) => {
+  try {
+    const userId = req.params.id.trim();
+    const newUsername = req.body.newUsername;
+    const updatedUser = await User.findByIdAndUpdate(userId, { username: newUsername }, { new: true });
+
+    if (updatedUser) {
+      res.send('Kullanıcı adı başarıyla güncellendi.');
+    } else {
+      res.status(404).send('Kullanıcı bulunamadı.');
+    }
+  } catch (err) {
+    console.error('Kullanıcı adı güncelleme hatası:', err);
+    res.status(500).send('Kullanıcı adı güncellenirken bir hata oluştu.');
+  }
+});
+
+
+
+module.exports = router;
